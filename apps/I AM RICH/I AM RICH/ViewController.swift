369a6ad2f4547633bf9b8ce2f3fbdf4fa3c4dc31
rickytorrez <torrez.ricky@ymail.com>
2018-12-04 21:35:40 -0500

    first commit

---
+++ apps/I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Ricky Torrez on 12/4/18.
+//  Copyright © 2018 E. Ricardo Torrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
