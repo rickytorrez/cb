eec10b75aba6f31eb00075ee8bf02ab25138602f
rickytorrez <torrez.ricky@ymail.com>
2019-01-22 18:15:00 -0500

    Data Types

---
+++ playground/Variables, constants and data types.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var myAge : Int = 29
+
+myAge = 33
+
+// Constant = Let
+let myName : String = "Ricardo"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = myName + " Torrez"
+
+let myFullName1 = "\(myFullName) Murillo"
+
+let myInformation = "\(myName), \(myAge)"
+
+let wholeNumbers : Int = 12
+
+let text : String = "abc"
+
+let booleans : Bool = true
+
+let floatingPointNumber : Float = 1.3
+
+let double : Double = 3.1415926
