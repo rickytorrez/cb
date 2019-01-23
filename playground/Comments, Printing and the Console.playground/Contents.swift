1a0e6e118c3594164744d8d7230348846b5c9e74
rickytorrez <torrez.ricky@ymail.com>
2019-01-23 12:23:42 -0500

    Magic 8 ball

---
+++ playground/Comments, Printing and the Console.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: This is a comment
+
+/* This
+ is
+ also
+ a
+ comment */
+
+import UIKit
+
+// Swift needs to be symmetrical in regards of spaces
+var monsterHealth = 19
+print(monsterHealth)
+
+monsterHealth = monsterHealth + 20
+print(monsterHealth)
+
+monsterHealth = monsterHealth / 3
+print(monsterHealth)
