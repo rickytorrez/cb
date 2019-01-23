cc6997e480ae7806da8e183c33dcaf1e4c7d07f6
rickytorrez <torrez.ricky@ymail.com>
2019-01-23 12:36:42 -0500

    Functions

---
+++ playground/Functions.playground/Contents.swift
@@ -0,0 +1,18 @@
+/*
+    They let you take a large number of instructions and package it into a function
+ 
+ */
+
+//: Creating a function
+func getMilk(){
+    print("go to the shops")
+    print("buy 2 cartons of milk")
+    print("pay $2")
+    print("come home")
+    
+}
+
+//: Calling the function above
+getMilk()
+
+
