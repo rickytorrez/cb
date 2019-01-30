8d3b9c4fceeb0846eb5d182fb8f29445676b86e5
rickytorrez <torrez.ricky@ymail.com>
2019-01-29 21:16:14 -0500

    Completed Xylo

--- apps/Xylophone/Xylophone/ VIewController.swift
+++ apps/Xylophone/Xylophone/ VIewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-//: Import Audio and Visual Foundation
+//: Import AVFoundation - Package that allows us to tap into the audio visual components in the iPhone
 import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController {
     
-    //: The question mark declares that it is an optional
-    var player: AVAudioPlayer?
+    var audioPlayer : AVAudioPlayer!
+    
+    //: We create a global variable named selectedSoundFileName that is an empty string
+    var selectedSoundFileName : String = ""
+    
+    //: Array of soundfile names - no need for file extensions
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,19 +29,32 @@ class ViewController: UIViewController{
     //: sender is the button that triggers the IBAction
     @IBAction func notePressed(_ sender: UIButton) {
         
-        //: Create a constant and a path to point where the sound is located in your project, don't forget the exclamation mark at the end
-        let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+        //: We recall the global varianle named selectedSoundFile and set it to a new value which equals the new sound we want to play. We use the sender.tag to pick a sound file name form the soundArray. The -1 fixes the index out of range bug in the array
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        
+        print(selectedSoundFileName)
+        
+        //: Calls on the playsound function
+        playSound()
+    }
     
-        //: Do catch block
+    func playSound() {
+        //: Sets up the soundURL to become the selectedSoundFileName resource that we need per each tag
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
         do {
-            player = try AVAudioPlayer(contentsOf: url)
-            guard let player = player else { return }
             
-            player.prepareToPlay()
-            player.play()
-        } catch let error as Error {
+            //: Try to initialize our autio player with the contents of the soundURL
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            
+            //: print the errors
             print(error)
         }
+        
+        //: After we do the try & catch block, we are gonna try to play the audio
+        audioPlayer.play()
     }
 }
 
